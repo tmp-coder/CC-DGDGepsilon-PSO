d795106e054227f00e5c7c810ec6ad59f1ebfb0b
taotao <zouzhitao_cq@163.com>
2018-01-16 20:26:19 +0800

    ....

--- code/test.m
+++ code/test.m
@@ -1,29 +1,15 @@
 % test file
 
 %test calc_diff function%
-
+tic;
 addpath(genpath(pwd))
 costf = 'F4';
-nvar=32;
+nvar=1000;
 LB = -100*ones(nvar,1);
 UB = 100*ones(nvar,1);
-acc_conn = 38;
-max_FEs = 1e5;
+acc_conn = nvar-ceil(nvar/20)+1;
+max_FEs = 3e6;
+t1 = clock;
 [best, cache_fit1,best_epsi,ng,FEs1,diff]= cc_gdg_pso(costf,nvar,LB,UB,max_FEs,acc_conn);
-
-
-[GBest,cache_fit2,FEs2] = test_pso();
-
-% test pso function
-function [GBest,cache_fit,FEs] = test_pso()
-
-    addpath(genpath(pwd))
-    costf = 'F4';
-    nvar=40;
-    LB = -100*ones(nvar,1);
-    UB = 100*ones(nvar,1);
-    groups = true(1,nvar);
-    max_FEs = 1e5;
-    [GBest,cache_fit,FEs] = pso(groups,max_FEs,costf,LB,UB);
-    
-end
+t2 = clock;
+disp(['cc_gdg_pso run time:',num2str(etime(t2,t1))]);
