90ae20517b3eac34d98d966dca3a205e7ec3ce3b
taotao <zouzhitao_cq@163.com>
2018-01-16 20:26:43 +0800

    add test standard pso

---
+++ code/test_stdpso.m
@@ -0,0 +1,18 @@
+% test std pso
+tic;
+t1 = clock;
+[GBest,cache_fit2,FEs2] = test_pso();
+t2 = clock;
+disp(['standard pso runtime: ',num2str(etime(t2,t1))]);
+% test pso function
+function [GBest,cache_fit,FEs] = test_pso()
+
+    addpath(genpath(pwd))
+    costf = 'F4';
+    nvar=1000;
+    LB = -100*ones(nvar,1);
+    UB = 100*ones(nvar,1);
+    groups = true(1,nvar);
+    max_FEs = 3e6;
+    [GBest,cache_fit,FEs] = pso(groups,max_FEs,costf,LB,UB);
+end
